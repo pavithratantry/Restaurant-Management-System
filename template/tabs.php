9cffd046e38a9745ddefd791dd8320d25abaf16b
pavithratantry <88243805+pavithratantry@users.noreply.github.com>
2021-09-25 21:13:14 +0530

    Create tabs.php

---
+++ template/tabs.php
@@ -0,0 +1,35 @@
+
+<section id="tabs">
+  <div class="container">
+    <nav class="navbar fixed">
+      <ul class="nav nav-pills ">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page">MENU</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link hover" href="#chaats">Chaats</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#southindian">South-Indian</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#northindian">North-Indian</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#pizzas">Pizzas & Pastas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#burgers">Burgers & Fries</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#juices">Juices & Shakes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#beverages">Hot Beverages</a>
+        </li>
+
+      </ul>
+    </nav>
+  </div>
+</section>
